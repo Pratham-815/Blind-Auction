c50f70d113c688a867f063470999c051340e9c49
Pratham Agarwal <143127428+Pratham-815@users.noreply.github.com>
2024-02-15 12:35:28 +0000

    Ask if there are more bidders

--- main.py
+++ main.py
@@ -10,4 +10,11 @@ while not bidding_finished:
     name = input("Enter the name of bidder: ")
     bid_amount = int(input("Enter the bid amount: "))
     bids[name] = bid_amount
+    more_bidders = input("Are there more bidders ?: ").lower()
+    if more_bidders=="no":
+        bidding_finished = True
+        highest_bidder(bids)
+    else:
+        continue
+    
     
